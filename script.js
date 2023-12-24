43f883528cdc288d03bab4d7f747d1dc0a93fa6c
Chris <factsbyday@gmail.com>
2023-12-23 21:42:16 -0600

    HardWare Program

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const prompt = require("prompt-sync")();
 
-let WelcomePrompt = "Welcome to the Quiz"
+let WelcomePrompt = "Welcome to the Quiz "
+let points = 0;
+const NumberOfQuestions = 3;
+const Name = prompt("Enter your Nickname: ")
 
-console.log(WelcomePrompt)
+
+console.log(WelcomePrompt + Name);
 
 const question1 = prompt("What is the brain of the computer? : ");
 const CorrectAns1 = "CPU"
 
 if(question1.toUpperCase() === CorrectAns1) {
     console.log("You got it right!");
+    points++;
 } else {
     console.log("Incorrect")
 }
@@ -20,17 +25,22 @@ const CorrectAns2 = "wood"
 
 if(question2.toLowerCase() === CorrectAns2) {
     console.log("You got it right!");
+    points++;
 } else {
     console.log("Incorrect")
 }
 
 //* ques 3
 
-const question3 = prompt("Who invented computer? : ");
+const question3 = prompt("Bonus Question - Who invented computer? :");
 const CorrectAns3 = "chris"
 
 if(question3.toLowerCase() === CorrectAns3) {
     console.log("You got it right!");
+    points+2;
 } else {
     console.log("Incorrect")
 }
+
+let FinalScore= Math.round((points/NumberOfQuestions) * 100);
+console.log("Congratulation you got: " + FinalScore.toString() + "%");
